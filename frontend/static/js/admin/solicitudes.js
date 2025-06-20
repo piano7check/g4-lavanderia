6964a7a101a7dff041e9e81307e6d2176731d911
Galia Almaraz <galia.almaraz@uab.edu.bo>
2025-06-20 12:33:02 -0400

    arreglando la interfaz del reidente

--- frontend/static/js/admin/solicitudes.js
+++ frontend/static/js/admin/solicitudes.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                     </select>
                 </td>
                 <td class="p-3 border">
-                    <button class="bg-green-600 text-white px-3 py-1 rounded hover:bg-green-700 notificarBtn" data-id="${s.id}">
+                    <button class="bg-green-600 p-2 text-white px-3 py-1 rounded hover:bg-green-700 notificarBtn" data-id="${s.id}">
                         Notificar
                     </button>
                 </td>
